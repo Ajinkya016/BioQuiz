c0175b195c5f2fef845245dc8f9838d70d90973a
Ajinkya016 <77497555+Ajinkya016@users.noreply.github.com>
2022-06-10 07:11:55 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,162 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "what type of cells are found at tip of roots?",
+    answer: "Apical Meritematic tissues",
+    options: [
+      "Apical Meritematic tissues",
+      "Apical Permanent tissues",
+      "parenchyma",
+      "xylem Meritematic tissues"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What connects a bone to a muscle?",
+    answer: "Tendon",
+    options: [
+      "Muscle cell",
+      "fiber",
+      "Ligament",
+      "Tendon"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What type of muscle does not get tired soon?",
+    answer: "cardiac muscle",
+    options: [
+      "cardiac muscle",
+      "Straited muscle",
+      "Unstraited muscle",
+      "All of above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Padding of what under skin acts as an insulation for retaining body heat?",
+    answer: "Adipose tissue",
+    options: [
+      "Nervous tissue",
+      "blood tissue",
+      "Areolar tissue",
+      "Adipose tissue"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What has no blood vessels or nerves but is found between vertibrae?",
+    answer: "Cartilage",
+    options: [
+      "Muscle cusions",
+      "Cartilage",
+      "inter-spine support tissue",
+      "Ligament"
+    ]
+  },
+    {
+    numb: 6,
+    question: "What type of cells are present in linings of trachea?",
+    answer: "cilliated columnar epithelium",
+    options: [
+      "Squamous epithelium",
+      "cuboidal epithelium",
+      "cilliated columnar epithelium",
+      "glandular epithelium"
+    ]
+  },
+    {
+    numb: 7,
+    question: "Where can cambium meristem be found?",
+    answer: "bellow the bark",
+    options: [
+      "bellow the bark",
+      "bellow the xylem",
+      "in the bark",
+      "Diameter of bark"
+    ]
+  },
+    {
+    numb: 8,
+    question: "Sclerenchyma cells develop quite thick walls due to deposition of?",
+    answer: "lignin",
+    options: [
+      "Cytoplasm",
+      "lignin",
+      "cell membrane",
+      "protoplasm"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Name of the part located as x in the diagram? <br/> <img style='height:200px;' src='phloem.png'/>",
+    answer: "Sieve Plates",
+    options: [
+      "Sieve Plates",
+      "Companion cell",
+      "Phloem Parenchyma cell",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 10,
+    question: "The epidermis of leaves secrete?",
+    answer: "waxy water-proof material",
+    options: [
+      "Oxygen, but in day",
+      "Stomta fluid",
+      "waxy water-proof water",
+      "waxy water-proof material"
+    ]
+  },
+    {
+    numb: 11,
+    question: "Which muscle constitute 50% of body weight?",
+    answer: "Straited muscle",
+    options: [
+      "Muscle Of cardiac",
+      "Straited muscle",
+      "Unstraited muscle",
+      "All of the above :)"
+    ]
+  },
+    {
+    numb: 12,
+    question: "What is cell body of neuron called other than cyton?",
+    answer: "Perikaryon",
+    options: [
+      "Cytonal body",
+      "Neuro cyton",
+      "Perikaryon",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 13,
+    question: "Bone Consists of living cells called?",
+    answer: "osteoblasts",
+    options: [
+      "Bonelets",
+      "osteoblasts",
+      "intra bone pathogens",
+      "Red bone osteoblasts"
+    ]
+  },
+
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
